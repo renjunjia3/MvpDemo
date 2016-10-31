6aac2cada08163f87231a34dea6a58377bf2eb9e
renjunjia3 <renjunjia>
2016-10-31 15:01:29 +0800

    模拟保存图片到本地

--- app/src/main/java/com/scene/myapplication/SaveImageActivity.java
+++ app/src/main/java/com/scene/myapplication/SaveImageActivity.java
@@ -1,8 +1,8 @@
 package com.scene.myapplication;
 
+import android.os.Bundle;
 import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,26 +11,29 @@ import com.scene.myapplication.presenter.SaveImagePresenterImpl;
 import com.scene.myapplication.utils.ToastUtil;
 import com.scene.myapplication.view.SaveImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SaveImageActivity extends AppCompatActivity implements SaveImageView, View.OnClickListener {
+    @BindView(R.id.save)
+    Button save;
+    @BindView(R.id.image)
+    ImageView image;
+    @BindView(R.id.progressBar)
+    ContentLoadingProgressBar progressBar;
+
     private SaveImagePresenterImpl saveImagePresenterImpl;
-    private ImageView imageView;
-    private ContentLoadingProgressBar progressBar;
-    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_image);
+        ButterKnife.bind(this);
         initView();
     }
 
     private void initView() {
         saveImagePresenterImpl = new SaveImagePresenterImpl(this);
-
-        imageView = (ImageView) findViewById(R.id.image);
-        progressBar = (ContentLoadingProgressBar) findViewById(R.id.progressBar);
-        save = (Button) findViewById(R.id.save);
-
         save.setOnClickListener(this);
     }
 
@@ -55,13 +58,13 @@ public class SaveImageActivity extends AppCompatActivity implements SaveImageVie
     }
 
     @Override
-    protected void onDestroy() {
-        saveImagePresenterImpl.destory();
-        super.onDestroy();
+    public void onClick(View v) {
+        saveImagePresenterImpl.saveImage(image);
     }
 
     @Override
-    public void onClick(View v) {
-        saveImagePresenterImpl.saveImage(imageView);
+    protected void onDestroy() {
+        saveImagePresenterImpl.destory();
+        super.onDestroy();
     }
 }
